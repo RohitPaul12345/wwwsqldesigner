91e8d6982eb3e98356c88282343608c671f8cda3
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-23 00:27:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-var CONFIG = {
+let CONFIG = {
     AVAILABLE_DBS: [
         "mysql",
         "sqlite",
@@ -85,14 +85,14 @@ var CONFIG = {
     XHR_PATH: "",
 
     /*
-     * The key below needs to be set individually by you if you want to use the Dropbox load/save feature.
-     * To do that, first sign up with Dropbox (may require a specific developer / SDK sign-up), go to
-     * https://www.dropbox.com/developers/apps and use "Create app" to add a new "Dropbox API app".
-     * Limit the app to its own folder. Call it, for instance, "wwwsqldesigner".
-     * Under "OAuth 2", "Redirect URIs", add the URL to the "dropbox-oauth-receiver.html" file on your server.
-     * E.g, if you install wwwsqldesigner on your local web server under "http://localhost/sqldesigner/", then add
-     * http://localhost/sqldesigner/dropbox-oauth-receiver.html as a Redirection URI.
-     * Copy the shown "App key" and paste it here below instead of the null value:
+      The key below needs to be set individually by you if you want to use the Dropbox load/save feature.
+      To do that, first sign up with Dropbox (may require a specific developer / SDK sign-up), go to
+      https://www.dropbox.com/developers/apps and use "Create app" to add a new "Dropbox API app".
+      Limit the app to its own folder. Call it, for instance, "wwwsqldesigner".
+      Under "OAuth 2", "Redirect URIs", add the URL to the "dropbox-oauth-receiver.html" file on your server.
+      E.g, if you install wwwsqldesigner on your local web server under "http://localhost/sqldesigner/", then add
+       http://localhost/sqldesigner/dropbox-oauth-receiver.html as a Redirection URI.
+       Copy the shown "App key" and paste it here below instead of the null value:
      */
     DROPBOX_KEY: null, // such as: "d6stdscwewhl6sa"
 };
